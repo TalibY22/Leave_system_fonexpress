0ae04c24b24ddcc90b4c591de5a020f1e9302dc8
TalibY22 <hzhzhhsj58@gmail.com>
2024-07-07 08:57:09 +0300

    password_change added

--- login/urls.py
+++ login/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from django.urls import path,include
+from . import views
 from django.contrib.auth.views import LoginView,LogoutView
 
 
 
 urlpatterns = [
      path('login/', LoginView.as_view(), name='login'),
-      path('logout/', LogoutView.as_view(), name='logout'),
+     path('logout/', LogoutView.as_view(), name='logout'),
+     path('password/', views.change_password, name='change_password'),
 ]
